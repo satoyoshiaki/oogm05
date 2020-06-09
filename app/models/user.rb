9c148535353542703ee01156d95a280a0847ecfc
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-09 14:59:48 +0900

    userのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+  validates :name, presence: true
+  validates :profile, length: { maximum: 250 }
 end
