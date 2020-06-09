43c39671d74eef99054d3dad1aefd0f36af273f7
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-09 16:32:00 +0900

    user/show画面の作成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,8 +38,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
+
+  def after_update_path_for(resource)
+    user_path(id: current_user.id)
+  end
+
+  def after_sign_in_path_for(resource)
+    recruitments_path
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root 'recruitment#index'
-  devise_for :users
+
+  #devise_for :users
   resources :recruitments
+
+  resources :users, only: [:show]
+
+  # ログイン、アカウント編集後、任意のページに推移させるための記述
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+  }
 end
