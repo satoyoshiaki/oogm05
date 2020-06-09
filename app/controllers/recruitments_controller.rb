41967c380380dca23c1586c15186647c21c113a6
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-09 13:55:26 +0900

    検索機能と絞り込み検索の実装

--- app/controllers/recruitments_controller.rb
+++ app/controllers/recruitments_controller.rb
@@ -4,6 +4,18 @@ class RecruitmentsController < ApplicationController
         @recruitments = Recruitment.all
         @recruitment = Recruitment.new
         # @recruitment = current_user.recruitments.build
+
+        if params[:search].present?
+            if params[:game_title].present? and params[:play_now].present?
+              @recruitments = @recruitments.game_title_search params[:game_title]
+              @recruitments = @recruitments.play_now_search params[:play_now]
+            elsif params[:game_title].present?
+              @recruitments = @recruitments.game_title_search params[:game_title]
+            elsif params[:play_now].present?
+              @recruitments = @recruitments.play_now_search params[:play_now]
+            end
+        end
+
       end
       def create
         # @recruitment = current_user.recruitments.build(recruitment_params)
@@ -14,10 +26,13 @@ class RecruitmentsController < ApplicationController
           render :edit
         end
       end
+
       def show
       end
+
       def edit
       end
+
       def update
         if @recruitment.update(recruitment_params)
           redirect_to recruitments_path, notice: "募集を編集しました！"
@@ -25,14 +40,17 @@ class RecruitmentsController < ApplicationController
           render :edit
         end
       end
+
       def destroy
         @recruitment.destroy
         redirect_to recruitments_path, notice:"募集を削除しました！"
       end
+
       private
       def recruitment_params
         params.require(:recruitment).permit(:play_now, :game_title, :comment, :machine, :image, :on_off, :purpouse )
       end
+
       # idをキーとして値を取得するメソッドを追加
       def set_recruitment
         @recruitment = Recruitment.find(params[:id])
--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -14,4 +14,7 @@ class Recruitment < ApplicationRecord
     enum machine: { PC: 0, スマホ: 1, swhich: 2, PS4: 3, PSVITA: 4, PS5: 5, カードゲーム: 6, xboxone: 7, その他: 8 }
     enum on_off: { オンライン: 0, オフライン: 1 }
     enum purpouse: { フレンド: 0, 協力プレイ: 1, 対戦: 2, 練習: 3, 経験値稼ぎ: 4, イベント周回: 5, イベント参加: 6, 大会: 7, 楽しくお話: 8, その他の目的: 9 }
+
+    scope :game_title_search, ->(game_title){where("game_title like?", "%#{game_title}%")}
+    scope :play_now_search, ->(play_now){where(play_now: play_now)}
 end
