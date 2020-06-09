4c7a769b533c34c4b57fd6e50b8e799595cfe072
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-09 17:16:30 +0900

    devise機能実装完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,11 @@ class ApplicationController < ActionController::Base
     # deviseコントローラーにストロングパラメータを追加する
     before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def after_sign_in_path_for(resource)
-      ‘/posts’
-    end
-
     protected
     def configure_permitted_parameters
       # サインアップ時にnameのストロングパラメータを追加
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
       # アカウント編集の時にnameとcommentのストロングパラメータを追加
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :comment])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :comment, :icon])
     end
 end
