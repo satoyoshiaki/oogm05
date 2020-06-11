487b6292f95ccc450b10a7866c92416410f4c707
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-11 18:32:45 +0900

    同じユーザーを二度フォローできないようにする

--- db/migrate/20200611085657_create_friendships.rb
+++ db/migrate/20200611085657_create_friendships.rb
@@ -1,10 +1,12 @@
 class CreateFriendships < ActiveRecord::Migration[5.2]
   def change
-    create_table :friendships do |t|
+    create_table :relationships do |t|
       t.integer :follower_id
       t.integer :followed_id
-
       t.timestamps
     end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true
   end
 end
