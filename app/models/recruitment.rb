66c5a4073998eccb9fe496510631417024e06234
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-11 12:05:32 +0900

    アソシエーションの作成

--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -20,4 +20,5 @@ class Recruitment < ApplicationRecord
     scope :play_now_search, ->(play_now){where(play_now: play_now)}
     has_many :favorites, dependent: :destroy
     has_many :favorite_users, through: :favorites, source: :user
+    has_many :comments, dependent: :destroy
 end
