00fc997e0370d2ba38ac1603faf4dbae7ee698cd
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-11 17:09:29 +0900

    コメント機能の実装完了

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,21 @@
 class CommentsController < ApplicationController
-    def create
-        @comment = Comment.new(comment_params)
+
+
+
+
+
+  def create
+        @recruitment = Recruitment.find(params[:recruitment_id])
+        @comment = @recruitment.comments.build(comment_params)
         @comment.user_id = current_user.id
         if @comment.save
-          redirect_back(fallback_location: root_path)
+          redirect_back(fallback_location: recruitments_path)
         else
-          redirect_back(fallback_location: root_path)
+          redirect_back(fallback_location: recruitments_path)
         end
     end
       private
       def comment_params
-        params.require(:comment).permit(:content)
+        params.require(:comment).permit(:content, :user_id, :recruitment_id)
       end
 end
