32ecd01e33a532999796ebfdfa2ea391b2c3bbb9
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-24 17:04:37 +0900

    LetterOpenerのルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -39,4 +39,6 @@ Rails.application.routes.draw do
   resources :conversations do
     resources :messages
   end
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
