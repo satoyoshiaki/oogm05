addcb6add0ffa428bad2d7b9a3d8c9777de53688
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-24 16:44:39 +0900

    メール文の作成

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,6 @@
 class ContactMailer < ApplicationMailer
+    def contact_mail(contact)
+        @contact = contact
+        mail to: "yoshiprograming@gmail.com", subject: "メッセージが届いていますよ!"
+      end
 end
