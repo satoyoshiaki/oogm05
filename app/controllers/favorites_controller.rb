367c2bc467bae54e2ffd3e4f9ee3ac8d4a583fa9
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-25 01:14:30 +0900

    お気に入りの時のリダイレクト先を変更

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,11 @@
 class FavoritesController < ApplicationController
     def create
         favorite = current_user.favorites.create(recruitment_id: params[:recruitment_id])
-        redirect_to recruitments_url
+        redirect_to recruitment_path(favorite.recruitment.id)
+
       end
       def destroy
         favorite = current_user.favorites.find_by(id: params[:id]).destroy
-        redirect_to recruitments_url
+        redirect_to recruitment_path(favorite.recruitment.id)
       end
 end
