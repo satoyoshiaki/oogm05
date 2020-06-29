3cecf9114bf5226c9d6124e0069505b42304e834
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-29 13:08:13 +0900

    herokuでドロップダウンが動かない

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap-sprockets
 //= require jquery/dist/jquery.js
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require bootstrap
-
+//= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
