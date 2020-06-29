41803525f5c5d8f2239c603d922df4fced446188
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-29 20:38:57 +0900

    herokuデプロイ

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,14 +16,3 @@
       provider: ""
     )
   end
-
-
-  User.create!(
-  name: "admin_user",
-  email: "admin_user@examp.com",
-  password: "adminpassword",
-  password_confirmation: "adminpassword",
-  uid: "812345",
-  provider: "",
-  admin: 'true'
-)
