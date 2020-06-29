a087013e218e794701139a6e8b0235aefdd0589f
佐藤良昭 <yoshiprograming@gmail.com>
2020-06-29 13:46:57 +0900

    ドロップダウン実験

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require bootstrap-sprockets
 //= require jquery/dist/jquery.js
 //= require rails-ujs
+//= require jquery
 //= require activestorage
 //= require turbolinks
 //= require bootstrap
